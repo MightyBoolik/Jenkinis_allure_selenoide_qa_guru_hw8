a7f13f71da385bc623abdbf07a8038f8fa91b227
MightyBoolik <bkuchaev@gmail.com>
2021-02-26 01:14:19 +0500

    Some fix in Test Base

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -23,8 +23,7 @@ public class TestBase {
             capabilities.setCapability("enableVNC", true);
             //capabilities.setCapability("enableVideo", true);
             Configuration.browserCapabilities = capabilities;
-            Configuration.remote = "https://user1:1234@https://selenoid.autotests.cloud:4444/wd/hub/";
-                    //System.getProperty("remote_driver");
+            Configuration.remote = System.getProperty("remote_driver");
 
         }
     }
